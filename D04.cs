e015290200e9b2b8a86ede92b16f0f2f58ca2b8b
Carlos.Alejo <Carlos.Alejo@bestsecret.com>
2025-12-04 07:35:13 +0100

    Day 4

---
+++ D04.cs
@@ -0,0 +1,72 @@
+﻿namespace AdventOfCode2025
+{
+    internal class D04 : Day
+    {
+        internal override void SolvePart1()
+        {
+            var map = FileHelper.ReadLinesAsCharMap("D04.txt");
+
+            long result = 0;
+            for (int i = 0; i < map.Length; i++)
+            {
+                for (int j = 0; j < map[i].Length; j++)
+                {
+                    if (map[i][j] != '@')
+                    {
+                        continue;
+                    }
+                    var p = new Position(j, i);
+                    var adjacentPositions = MatrixHelper.GetAdjacentPositions(p, map[i].Length, map.Length);
+                    var adjacentRolls = adjacentPositions.Where(pos => map[pos.Y][pos.X] == '@').Count();
+                    if (adjacentRolls < 4)
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        internal override void SolvePart2()
+        {
+            var map = FileHelper.ReadLinesAsCharMap("D04.txt");
+
+            long total = 0;
+            long removedRolls;
+            do
+            {
+                removedRolls = 0;
+                var rollPositions = new List<Position>();
+                for (int i = 0; i < map.Length; i++)
+                {
+                    for (int j = 0; j < map[i].Length; j++)
+                    {
+                        if (map[i][j] != '@')
+                        {
+                            continue;
+                        }
+                        var p = new Position(j, i);
+                        var adjacentPositions = MatrixHelper.GetAdjacentPositions(p, map[i].Length, map.Length);
+                        var adjacentRolls = adjacentPositions.Where(pos => map[pos.Y][pos.X] == '@').Count();
+                        if (adjacentRolls < 4)
+                        {
+                            rollPositions.Add(p);
+                            removedRolls++;
+                        }
+                    }
+                }
+                if (removedRolls > 0)
+                {
+                    total += removedRolls;
+                    foreach (var p in rollPositions)
+                    {
+                        map[p.Y][p.X] = '.';
+                    }
+                }
+            } while (removedRolls > 0);
+
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ MatrixHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025
+{
+    internal class MatrixHelper
+    {
+        public static readonly (int x, int y)[] Directions =
+        [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),           (0, 1),
+            (1, -1),  (1, 0),  (1, 1)
+        ];
+
+        public static Position[] GetAdjacentPositions(Position pos, int maxX, int maxY)
+        {
+            var positions = new List<Position>();
+            foreach (var (dx, dy) in Directions)
+            {
+                int newX = pos.X + dx;
+                int newY = pos.Y + dy;
+                if (newX >= 0 && newX < maxX && newY >= 0 && newY < maxY)
+                {
+                    positions.Add(new Position(newX, newY));
+                }
+            }
+            return [.. positions];
+        }
+    }
+
+    internal class Position(int x, int y)
+    {
+        public int X { get; set; } = x;
+        public int Y { get; set; } = y;
+    }
+}
